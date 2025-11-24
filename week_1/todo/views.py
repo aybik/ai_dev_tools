4170dfdd9bc7bc4097bb721f0015045ba5849b2b
aybike alkan <aybike.alkn@gmail.com>
2025-11-24 12:47:27 +0100

    implement logic in views.py

--- week_1/todo/views.py
+++ week_1/todo/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Todo
+from .forms import TodoForm
 
-# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all().order_by('due_date', 'resolved')
+    return render(request, 'home.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo_form.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo_form.html', {'form': form})
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todo_confirm_delete.html', {'todo': todo})
+
+def todo_resolve(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.resolved = True
+    todo.save()
+    return redirect('todo_list')
